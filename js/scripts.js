e30e39fc9ead7056d4ab38413f6ee072e34e7db4
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-27 15:17:15 -0700

    Add alerts for empty user input

--- js/scripts.js
+++ js/scripts.js
@@ -20,21 +20,36 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
     var day = $("input:radio[name=day]:checked").val();
-    // var todo = $("input:radio[name=todo]:checked").val();
     var purpose = $("input:radio[name=purpose]:checked").val();
+      if (age <= 7) {
+        alert("You must be over 7 years old")
+        return;
+      } else if ($("#age").val() == "") {
+        alert("Enter you age, please");
+        return;
+      } else if ($("#name").val() == "") {
+        alert("Enter you name, please");
+        return;
+      }
+      
 
     console.log(name, age, gender, day, purpose)
-    //if (age <= 7){
-      //alert("You must be over 7 years old")
-      //return;
-    //}
+    
     if ($("#prefer").val() === "website") {
-    $("#javascript").removeClass("hidden");
-  } else if ($("#prefer").val() === "apple") {
-    $("#swift").removeClass("hidden");
-  } else {
-    $("#python").removeClass("hidden");
-  }
+      $("#javascript").removeClass("hidden");
+      $("#name").show();
+    } else if ($("#prefer").val() === "apple") {
+      $("#swift").removeClass("hidden");
+      $("#name").show();
+    } else {
+      $("#python").removeClass("hidden");
+    }
+
+
+
+  
+
+
   
 
     
